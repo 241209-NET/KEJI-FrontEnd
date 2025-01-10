c3717a65dadfa85b367b42b47ecf9258684db157
Isaac Hessel-Robinson <isaacHhyphenR@gmail.com>
2025-01-10 17:45:00 -0600

    Restructured pages into new folder

--- Finance-App/src/App.jsx
+++ Finance-App/src/App.jsx
@@ -1,14 +1,15 @@
 import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
-import Intro from './intro/intro.jsx';
-import Login from './register/login/login.jsx';
-import SignUp from './register/signup/signup.jsx';
-import Reset from './register/reset/reset.jsx';
-import Homepage from './homepage/homepage.jsx';
-import Account from './Account/Account.jsx'
-import Activity from './Transaction/Transaction.jsx'
-// import Statement from './Statement/Statement.jsx';
-// import TransactionDetails from './transactionDetail/transactionDetail.jsx';
-// import Goals from './ Goals/Goal.jsx';
+import Intro from './Pages/intro/intro.jsx';
+import Login from './Pages/register/login/login.jsx';
+import SignUp from './Pages/register/signup/signup.jsx';
+import Reset from './Pages/register/reset/reset.jsx';
+import Homepage from './Pages/homepage/homepage.jsx';
+import Account from './Pages/Account/Account.jsx'
+import Activity from './Pages/Transaction/Transaction.jsx';
+import Statement from './Pages/Statement/Statement.jsx';
+import Transaction from './Pages/Transaction/Transaction.jsx';
+import TransactionDetails from './Pages/transactionDetail/transactionDetail.jsx';
+//import Goals from './Pages/Goals/Goal.jsx';
 
 function App() {
 
@@ -16,13 +17,15 @@ function App() {
 
     <Router>
       <Routes>
-        <Route path="/" element={<Intro />} />
-        <Route path="/login" element={<Login />} />
+        <Route path='/' element={<Intro />} />
+        <Route path='/login' element={<Login />} />
         <Route path='/signup' element={<SignUp />} />
         <Route path='/reset' element={<Reset />} />
         <Route path='/homepage' element={<Homepage />} />
         <Route path='/account' element={<Account />} />
         <Route path='/activity' element={<Activity />} />
+        <Route path='/Statement' element={<Statement />} />
+        <Route path='/Transaction' element={<Transaction />} />
       </Routes>
     </Router>
 
