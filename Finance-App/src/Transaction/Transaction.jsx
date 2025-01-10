2395e1722246e19a0e47aa7e0c019671fe7c7e14
Isaac Hessel-Robinson <isaacHhyphenR@gmail.com>
2025-01-10 09:49:55 -0600

    improved transaction styling

--- Finance-App/src/Transaction/Transaction.jsx
+++ Finance-App/src/Transaction/Transaction.jsx
@@ -6,28 +6,34 @@ const Transaction = () => {
     <div class = "transaction-container">
       <h2>Make New Deposit in Account #</h2>
       <form class = "transaction-form" id = "transaction-form">
-        <label for = "transaction-amount">Amount: </label>
-        <input type = "number" id = "transaction-amount" name = "transaction-amount" class = "input-box" placeholder = "0.00" required/>
-        <br/>
+        <div class = "input-line">
+          <label for = "transaction-amount">Amount: </label>
+          <input type = "number" id = "transaction-amount" name = "transaction-amount" class = "input-box" placeholder = "0.00" required/>
+        </div>
         
-        <label for = "transaction-merchant">Merchant: </label>
-        <input type = "text" name = "transaction-merchant" class = "input-box" placeholder = "Store" required/>
-        <br/>
+        <div class = "input-line">
+          <label for = "transaction-merchant">Merchant: </label>
+          <input type = "text" name = "transaction-merchant" class = "input-box" placeholder = "Store" required/>
+        </div>
         
-        <label for = "transaction-description">Description: </label>
-        <input type = "text" name = "transaction-description" class = "input-box" placeholder = "Food"/>
-        <br/>
+        <div class = "input-line">
+          <label for = "transaction-description">Description: </label>
+          <input type = "text" name = "transaction-description" class = "input-box" placeholder = "Food"/>
+        </div>
 
-        <label for = "transaction-date">Date: </label>
-        <input type = "date" name = "transaction-date" class = "input-box" required/>
-        <br/>
+        <div class = "input-line">
+          <label for = "transaction-date">Date: </label>
+          <input type = "date" name = "transaction-date" class = "input-box" style = {{width:95}} required/>
 
-        <label for = "transaction-recurring">Recurring: </label>
-        <input type = "radio" id="transaction-recurring-no" name="transaction-recurring" value="HTML" class = "radio-checkbox" required/>
-        <label for = "transaction-recurring-no">No</label>
-        <input type = "radio" id="transaction-recurring-yes" name="transaction-recurring" value="CSS" class = "radio-checkbox" required/>
-        <label for = "transaction-recurring-yes">Yes</label>
-        <br/>
+          <label for = "transaction-recurring">Recurring: </label>
+          <section class = "stacked-radio-answer-list">
+            <input type = "radio" id="transaction-recurring-no" name="transaction-recurring" value="No" required/>
+            <label for = "transaction-recurring-no" class = "transaction-radio-answer">No</label>
+            <br/>
+            <input type = "radio" id="transaction-recurring-yes" name="transaction-recurring" value="Yes" required/>
+            <label for = "transaction-recurring-yes" class = "transaction-radio-answer">Yes</label>
+          </section>
+        </div>
 
         <input type = "submit" value="Deposit"/>
       </form>
