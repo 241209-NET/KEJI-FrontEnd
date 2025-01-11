67f7befeca2375cd459825c0f16772e21b10e6d2
Isaac Hessel-Robinson <isaacHhyphenR@gmail.com>
2025-01-11 17:26:13 -0600

    Improved nav-bar formatting

--- Finance-App/src/Components/NavBar.jsx
+++ Finance-App/src/Components/NavBar.jsx
@@ -29,13 +29,13 @@ const NavBar = () => {
     return (
         <nav class="nav-bar">
             <ul class="nav-links">
+            <li><button onClick={handleAccount}>Account</button></li>
                 <li><button onClick={handleHome}>Home</button></li>
-                <li><button onClick={handleAccount}>Account</button></li>
+                <li><button onClick={handleTransaction}>Withdraw/Deposit</button></li>
                 <li><button onClick={handleActivity}>Activity</button></li>
                 <li><button onClick={handleStatement}>Statements</button></li>
-                <li><button onClick={handleTransaction}>Make Transaction</button></li>
-                <li><button onClick={handleGoals}>Goals</button></li>
                 <li><button onClick={handleExchange}>Exchange</button></li>
+                <li><button onClick={handleGoals}>Goals</button></li>
                 <li><img name = "logo" class = "logo" src={logo} alt="Logo"/><label for = "logo" class = "logo-label">Keji</label></li>
             </ul>
         </nav>
